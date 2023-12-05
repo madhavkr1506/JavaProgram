59a02e86ba3ed34c54cd6c88c80d639b1edfc13b
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2023-12-06 02:34:15 +0530

    Add files via upload

---
+++ GameStringArray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class GameStringArray {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int num=scanner.nextInt();
+        String[] array1=new String[num];
+        String[] array2=new String[num];
+        String[] array3=new String[num];
+        for(int i=0;i<num;i++){
+            array1[i]=scanner.next();
+        }
+        scanner.nextLine();
+        for(int i=num-1;i>=0;i--){
+            array2[i]=scanner.next();
+        }
+        scanner.nextLine();
+        for(int i=0;i<num;i++){
+            array3[i]=array1[i] + " " + array2[i];
+        }
+        for(int i=0;i<num;i++){
+            System.out.print(array3[i] + " ");
+        }
+    }
+}
+
