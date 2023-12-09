b16405e3bb82b7391ed32d7c7968b6fe9c8c486b
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2023-12-09 20:51:03 +0530

    Add files via upload

---
+++ KthSmallestElement.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class KthSmallestElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num=scanner.nextInt();
+        int[] array1=new int[num];
+        for(int i=0;i<num;i++){
+            array1[i]=scanner.nextInt();
+        }
+        int pos= scanner.nextInt();
+        findKthSmallestElement(array1,num,pos);
+        for(int element : array1){
+            System.out.print(element + " ");
+        }
+
+
+    }
+    static void findKthSmallestElement(int[] array,int num,int position){
+        for(int i=0;i<num-1;i++){
+            for(int j=0;j<num-i-1;j++){
+                if(array[j] < array[j+1]){
+                    int temp=array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=temp;
+                }
+            }
+        }
+        System.out.println(array[num-position]);
+    }
+}
