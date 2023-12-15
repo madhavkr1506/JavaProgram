d59766309207293d0b7b1034a13cf8e3e18b8483
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2023-12-15 13:19:51 +0530

    Add files via upload

--- Java/PackageUsing.java
+++ Java/PackageUsing.java
@@ -1,9 +1,18 @@
 import madhav.Hello;
 import madhav.HelloEarth;
+
+class A extends HelloEarth{
+    void print(){
+        // System.out.println(a);   //  ----> It will throw an error
+        System.out.println(b);
+    }
+}
 public class PackageUsing {
     public static void main(String[] args) {
         System.out.println("I am using Package.");
-        Hello h = new Hello();
-        HelloEarth he = new HelloEarth();
+        // Hello h = new Hello();
+        // HelloEarth he = new HelloEarth();
+        A obj1 = new A();
+        obj1.print();
     }
 }
--- Java/madhav/HelloEarth.java
+++ Java/madhav/HelloEarth.java
@@ -1,6 +1,10 @@
 package madhav;
 
+
+
 public class HelloEarth {
+    int a = 10;
+    protected int b = 11;
     public static void main(String[] args) {
         System.out.print("Hello and welcome!\n");
 
