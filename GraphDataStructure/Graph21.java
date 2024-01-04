91b9800b787db38f04b737ad987912ff729b804b
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-01-04 21:40:10 +0530

    Add files via upload

--- GraphDataStructure/Graph21.java
+++ GraphDataStructure/Graph21.java
@@ -1,10 +1,10 @@
-//Note : Cycle detection in directed graph
+//Note : Cycle detection for undirected graph.
 
 package GraphDataStructure;
 
 import java.util.ArrayList;
 
-public class Graph20 {
+public class Graph21 {
     static class Edge{
         int source;
         int destination;
@@ -13,55 +13,51 @@ public class Graph20 {
             this.destination=destination;
         }
     }
-    public static void createGraph(ArrayList<Edge> graph[]){
+    public static void creategraph(ArrayList<Edge> graph[]){
         for(int i=0;i< graph.length;i++){
-            graph[i] = new ArrayList<Edge>();
+            graph[i]=new ArrayList<Edge>();
         }
         graph[0].add(new Edge(0,1));
         graph[0].add(new Edge(0,2));
-
+        graph[1].add(new Edge(1,0));
         graph[1].add(new Edge(1,2));
-
-        graph[2].add(new Edge(2,3));
         graph[2].add(new Edge(2,0));
-
-        graph[3].add(new Edge(3,3));
+        graph[2].add(new Edge(2,1));
+        graph[2].add(new Edge(2,3));
+        graph[3].add(new Edge(3,2));
     }
-
-    public static boolean IsCycleUtil(ArrayList<Edge> graph[],int current,boolean vis[],boolean rec[]){
+    public static boolean IsCycleUtil(ArrayList<Edge> graph[],int current,boolean vis[],int parent){
         vis[current]=true;
-        rec[current]=true;
-        for(int i=0;i< graph[current].size();i++){
+        for(int i=0;i<graph[current].size();i++){
             Edge e = graph[current].get(i);
-            if(rec[e.destination]){
+            if(vis[e.destination] && parent != e.destination){
                 return true;
             }
-            else if(!vis[e.destination]){
-                if(IsCycleUtil(graph,e.destination,vis,rec)){
+            if(!vis[e.destination]){
+                if(IsCycleUtil(graph, e.destination, vis, current)){
                     return true;
                 }
             }
         }
-        rec[current]=false;
         return false;
     }
-    public static boolean isCycle(ArrayList<Edge> graph[]){
+    public static boolean IsCycle(ArrayList<Edge> graph[]){
         boolean vis[] = new boolean[graph.length];
-        boolean rec[] = new boolean[graph.length];
         for(int i=0;i< graph.length;i++){
             if(!vis[i]){
-                boolean IsCycle = IsCycleUtil(graph,i,vis,rec);
-                if(IsCycle){
+                boolean isCycle = IsCycleUtil(graph,i,vis,-1);
+                if(isCycle){
                     return true;
                 }
             }
         }
         return false;
     }
+    
     public static void main(String[] args) {
         int vertex = 4;
         ArrayList<Edge> graph[] = new ArrayList[vertex];
-        createGraph(graph);
-        System.out.println(isCycle(graph));
+        creategraph(graph);
+        System.out.println(IsCycle(graph));
     }
 }
