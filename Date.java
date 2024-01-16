2a15802d67454f3b98d2c82fec6aec7c465a2c67
Rjram-10 <118926956+Rjram-10@users.noreply.github.com>
2024-01-16 23:55:58 +0530

    Date.java

--- Date.java
+++ Date.java
@@ -4,5 +4,6 @@ public class Date {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println(System.currentTimeMillis() /1000/3600/365/24 );
+        //forked it
     }
 }
