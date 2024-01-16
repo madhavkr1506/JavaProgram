88fd89c00cc6ba48ba988f5acf4154284c274f26
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-01-17 00:22:02 +0530

    Add files via upload

--- Date.java
+++ Date.java
@@ -4,6 +4,5 @@ public class Date {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println(System.currentTimeMillis() /1000/3600/365/24 );
-        //forked it
     }
 }
