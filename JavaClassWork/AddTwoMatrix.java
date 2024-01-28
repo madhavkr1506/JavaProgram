ca06e6ff0844d78941d71a50b5a1fcfe52084d28
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-01-28 17:24:48 +0530

    Add files via upload

---
+++ JavaClassWork/AddTwoMatrix.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class AddTwoMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+        int[][] two_dim = new int[num][num];
+
+        for(int i = 0;i < num;i++){
+            for(int j = 0;j < num;j++){
+                two_dim[i][j] = scanner.nextInt();
+            }
+        }
+
+        for(int i = 0;i < num;i++){
+            for(int j = 0;j < num;j++){
+                System.out.print((two_dim[i][j] + two_dim[i][j]) + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
