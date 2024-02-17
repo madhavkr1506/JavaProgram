db8f1c102802e924721ae013e12ff45d444dc399
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-02-18 03:19:16 +0530

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,37 @@
+public class String1 {
+    public static void main(String[] args) {
+        // String name = new String();
+        // name = "Madhav";
+        // System.out.println(name);
+
+        // name.setCharAt(0,'m'); //--> It will give an error because string is not mutable.
+
+        // StringBuilder name = new StringBuilder(); //--> it is mutable in nature.
+        // name.append("Madhav");
+        // System.out.println(name);
+
+        // name.setCharAt(0, 'm');
+        // System.out.println(name);
+
+        // char c = 'm';
+        // String str = Character.toString(c);
+        // System.out.println(str.toUpperCase());
+
+        // String name = "abcdefghij";
+        // for (int i = 0; i < name.length(); i++) {
+        //     if(i % 2 != 0){
+        //         char c = name.charAt(i);
+        //         String str = Character.toString(c);
+        //         System.out.print(str.toUpperCase());
+        //     }
+        // }
+
+        // char c = 'M';
+        // System.out.println(Character.toLowerCase(c));
+
+        
+
+
+
+    }
+}
