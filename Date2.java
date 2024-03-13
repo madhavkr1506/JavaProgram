82b1ffbfee7e4b950813f4cb3cdfde367716a70d
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-03-13 23:08:16 +0530

    Add files via upload

---
+++ Date2.java
@@ -0,0 +1,22 @@
+// import java.util.*;
+
+public class Date2 {
+    public static void main(String[] args) {
+        // Date d1 = new Date(2000,11,21);
+        // Date d2 = new Date();
+        // Date d3 = new Date(2025,1,3);
+        
+
+        // boolean a = d3.after(d1);
+        // System.out.println("Date d3 comes after Date d1 : " + a);
+
+        // boolean b = d3.before(d2);
+        // System.out.println("Date d3 comes after Date d2 : " + b);
+
+        // int c = d1.compareTo(d2);
+        // System.out.println(c);
+
+
+
+    }
+}
