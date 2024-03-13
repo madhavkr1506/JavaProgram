f414ceae12ec0637037223a440ba5c560953e830
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-03-13 15:34:54 +0530

    Add files via upload

---
+++ Date1.java
@@ -0,0 +1,37 @@
+// import java.util.*;
+
+public class Date1 {
+    public static void main(String[] args) {
+        // Date class in java : 
+        // the class date represents a specific instant in time, with millisecond precision.
+        // the date class of java.util package implements serializable, cloneable and comparable interface.
+        // it proviode constructors and methods to deal with date and time with java.
+        // Date() : create date object representing current date and time.
+        // Date(long milliseconds) : creates a date object for the given milliseconds since january 1, 1970, 00:00:00 GMT.
+        // Date(int year, int month, int date)
+        // Date(int year, int month, int date, int hrs, int min)
+        // Date(int year, int month, int date, int hrs, int min, int sec)
+        // Date(String s)
+        // Note : in the last 4 constructors of the date class are deprecated.
+        // System.out.println(System.currentTimeMillis()/1000/3600/365/24);
+
+        // Date d = new Date();
+        // System.out.println(d);
+        // System.out.println(d.getTime()/1000/3600/365/24);
+        // System.out.println(d.getDate());
+        // System.out.println(d.getHours());
+        // System.out.println(d.getDay());
+        // System.out.println(d.getSeconds());
+
+        // System.out.println(d.getMonth());
+        // System.out.println(d.getMinutes());
+        // System.out.println(d.getYear());
+
+        // Date d1 = new Date(124,2,13,15,29);
+        // System.out.println(d1);
+
+
+
+
+    }
+}
