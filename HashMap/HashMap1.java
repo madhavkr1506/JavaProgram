0cb13689dca5f6f01404ce8d30dc95aa9073ccf7
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-04-11 01:32:45 +0530

    Add files via upload

---
+++ HashMap/HashMap1.java
@@ -0,0 +1,64 @@
+import java.util.HashMap;
+// import java.util.Map;
+
+public class HashMap1{
+    public static void main(String[] args) {
+        HashMap<String,String> hashMap = new HashMap<>();
+
+        hashMap.put("Afghanistan", "Kabul");
+        hashMap.put("Albania", "Tirane");
+        hashMap.put("Algeria", "Algiers");
+        hashMap.put("Andorra", "Andorra la vella");
+        hashMap.put("Angola", "Luanda");
+        hashMap.put("Antigua and Barbuda", "Saint John's");
+        hashMap.put("Argentina", "Buenos Aires");
+        hashMap.put("Armenia", "Yerevan");
+        hashMap.put("Australia", "Canberra");
+        hashMap.put("Austria", "Vienna");
+        hashMap.put("Azerbaijan", "Baku");
+        hashMap.put("Bharain", "Manama");
+        hashMap.put("Bangladesh", "Dhaka");
+        hashMap.put("Barbados", "Bridgetown");
+        hashMap.put("Belarus", "Minsk");
+        hashMap.put("Belgium", "Brussels");
+        hashMap.put("Bhutan", "Thimphu");
+        hashMap.put("Brazil", "Brasillia");
+        hashMap.put("Bulgaria", "Sofia");
+        hashMap.put("Canada", "Ottawa");
+        hashMap.put("China", "Beijing");
+
+        // System.out.println(hashMap);
+
+
+        // for(Map.Entry<String, String> e : hashMap.entrySet()){
+        //     System.out.println(e.getKey()+ " " + e.getValue());
+        // }
+
+        // System.out.println(hashMap.get("Canada"));
+
+
+        // hashMap.remove("Canada");
+        // System.out.println(hashMap.size());
+
+        // hashMap.clear();
+        // System.out.println(hashMap.size());
+
+        // print all keys : 
+        // for(String key : hashMap.keySet()){
+        //     System.out.println(key);
+        // }
+
+        // print all values : 
+        // for(String value : hashMap.values()){
+        //     System.out.println(value);
+        // }
+
+        // print key and value : 
+
+        // for(String str1 : hashMap.keySet()){
+        //     System.out.println(str1 + " " + hashMap.get(str1));
+        // }
+
+
+    }
+}
