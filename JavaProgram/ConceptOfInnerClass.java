74638dd62323648ff21bd761b59c1f32cde56abb
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-05-09 22:07:58 +0530

    Add files via upload

---
+++ JavaProgram/ConceptOfInnerClass.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+interface features{
+    void model(String string);
+    void mfgyear(LocalDate date);
+    void color(String string);
+    void fueltype(String string);
+    void mileage(int mileage);
+}
+
+class carPurchase implements features{
+    HashMap<String,String> garage = new HashMap<>();
+
+    public void model(String model_number){
+        if(garage.containsKey(model_number)){
+            private class CarDetails{
+
+            }
+        }
+    }
+}
+public class ConceptOfInnerClass{
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+    }
+}
