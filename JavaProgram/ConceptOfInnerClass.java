4876f46eb72935054a2e9ef78dc06adecc534ba5
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-05-10 21:10:02 +0530

    Add files via upload

--- JavaProgram/ConceptOfInnerClass.java
+++ JavaProgram/ConceptOfInnerClass.java
@@ -10,21 +10,21 @@ interface features{
     void mileage(int mileage);
 }
 
-class carPurchase implements features{
-    HashMap<String,String> garage = new HashMap<>();
 
-    public void model(String model_number){
-        if(garage.containsKey(model_number)){
-            private class CarDetails{
-
-            }
-        }
-    }
-}
 public class ConceptOfInnerClass{
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Input car details");
+        System.out.println("Enter model number : ");
+        String modelNumber = sc.nextLine();
+        System.out.println("Enter Manufacturing year : ");
+        String mfgYear = sc.nextLine();
+        LocalDate mfgDate = LocalDate.parse(mfgYear,DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        System.out.println("Enter color of the car : ");
+        String color = sc.nextLine();
+        System.out.println("Enter mileage : ");
+        int mileage = sc.nextInt(); 
         sc.close();
     }
 }
